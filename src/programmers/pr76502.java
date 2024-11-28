15fc529f84a7d484dbf66248cba3298b7dec1030
eunsueunsu <imimim93@gmail.com>
2024-11-28 15:39:55 +0900

    pr 87390 clear

--- src/programmers/pr76502.java
+++ src/programmers/pr76502.java
@@ -1,2 +1,48 @@
-package programmers;public class pr76502 {
+package programmers;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class pr76502 {
+
+    /* 괄호 회전하기
+    *
+    *         // 순회 - 시작값이 end괄호이면 Continue
+        // else - 스택에 담고 다음값과 짝이 맞으면 제거
+        // 남은 값이 없으면 success case
+    * */
+    public static void main(String[] args) {
+        solution("}]()[{");
+    }
+
+    public static int solution(String s) {
+        int answer = 0;
+
+        String end = ")}]";
+
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        // end를 키로 갖는 map의 value 와 비교
+        for (int i = 0; i < s.length(); i++) {
+            Stack<Character> stack = new Stack<Character>();
+            if (end.contains(String.valueOf(s.charAt(i)))) continue;
+            stack.push(s.charAt(i));
+            for (int j = 1; j < s.length(); j++) {
+                char next = s.charAt((i + j) % s.length());
+                if (stack.empty()) {
+                    stack.push(next);
+                } else if (map.get(next) == stack.peek()) {
+                    stack.pop();
+                } else {
+                    stack.push(next);
+                }
+            }
+            if (stack.empty()) answer++;
+        }
+
+        return answer;
+    }
 }
--- src/programmers/pr87390.java
+++ src/programmers/pr87390.java
@@ -1,2 +1,43 @@
-package programmers;public class pr87390 {
+package programmers;
+
+public class pr87390 {
+
+
+    public static void main(String[] args) {
+
+        solution(3, 2, 5);
+    }
+
+    public static int[] solution(int n, long left, long right) {
+        int[] answer = {};
+/*
+*
+ 인덱스를 좌표로 변경
+ * 해당 좌표에 할당된 숫자 계산 ( x,y로 계산)
+*
+* */
+        answer = new int[(int) (right - left + 1)];
+        int index = 0;
+        while (true) {
+            if (left > right) break;
+            answer[index] = getNum(left, n);
+            left++;
+            index++;
+        }
+        return answer;
+    }
+
+    public static int getNum(Long n, int length) {
+
+        int result = 0;
+
+        int y = (int) (n % length);
+        int x = (int) (n / length);
+
+        if (x >= y) result = x + 1;
+        else result = y + 1;
+
+        return result;
+    }
+
 }
